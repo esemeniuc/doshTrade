2ac6cf12e1e973f62ed30fa67c62a344acb95403
kai wen zhang <kaizhangwen@gmail.com>
2020-06-10 15:18:28 -0700

    create basic layout view

--- client/src/components/StockListView.tsx
+++ client/src/components/StockListView.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
 import Container from "@material-ui/core/Container";
-import {Box, Paper} from "@material-ui/core";
+import {
+    AppBar,
+    Avatar,
+    Box, Chip,
+    List,
+    ListItem,
+    ListItemAvatar,
+    ListItemSecondaryAction,
+    ListItemText,
+    Paper, Toolbar, Typography
+} from "@material-ui/core";
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -10,6 +20,11 @@ const useStyles = makeStyles((theme: Theme) =>
         },
         menuButton: {
             marginRight: theme.spacing(2),
+        },
+        list: {
+            marginTop: theme.spacing(4),
+            width: '100%',
+            backgroundColor: theme.palette.background.paper
         }
     }),
 );
@@ -18,13 +33,44 @@ function StockListView() {
     const classes = useStyles();
 
     return (
-        <div className={classes.root}>
-            <Container component="main">
-                <Paper>
-                    <Box p={3}>
-                        stock list view
-                    </Box>
-                </Paper>
+        <div className={classes.root} >
+            <Container component="main" maxWidth='sm'>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography variant="h6" >
+                            Yolo Trader
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                    <Typography variant="caption" >
+                        <Box textAlign="center" >
+                            updated since june 10, 2020 12:00:15PM
+                        </Box>
+                    </Typography>
+                    <List className={classes.list}>
+                        <ListItem button>
+                            <ListItemText id='{1}' primary='TSLA' secondary='Up 7.86%'/>
+                            <ListItemSecondaryAction>
+                                <Chip label="1011.87" color='primary'/>
+                            </ListItemSecondaryAction>
+                        </ListItem>
+
+                        <ListItem button>
+                            <ListItemText id='{2}' primary='SQ' secondary='Up 2.51'/>
+                            <ListItemSecondaryAction>
+                                <Chip label="92.12" color='primary'/>
+                            </ListItemSecondaryAction>
+                        </ListItem>
+
+                        <ListItem button>
+                            <ListItemText id='{3}' primary='SPY' secondary='Up 0.12'/>
+                            <ListItemSecondaryAction>
+                                <Chip label="320.87" color='primary'/>
+                            </ListItemSecondaryAction>
+                        </ListItem>
+
+
+                    </List>
             </Container>
         </div>
     );
