84990e1de6c61d134e489643daaf23298cc04e20
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2020-06-12 14:48:53 -0700

    wip to convert library

--- server/src/handler.rs
+++ server/src/handler.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 use std::borrow::Cow;
 use rust_embed::RustEmbed;
-use actix_web::{web, Error, HttpRequest, HttpResponse};
+use actix_web::{web, Error, HttpRequest, HttpResponse,Result};
 use actix_web::body::Body;
 use actix_web::http::header::Header;
 use actix_web_httpauth::headers::authorization::{Authorization, Bearer};
@@ -9,109 +9,80 @@ use crate::db::DbPool;
 use crate::graphql;
 use crate::models::{Event};
 
+use async_graphql::http::{playground_source, GraphQLPlaygroundConfig};
+use async_graphql_actix_web::{GQLRequest, GQLResponse, WSSubscription};
+use actix_web_actors::ws;
+use crate::asyncgql::{BooksSchema, MutationRoot, QueryRoot, Storage, SubscriptionRoot};
+
 //from https://github.com/pyros2097/rust-embed/blob/master/examples/actix.rs
-#[derive(RustEmbed)]
-#[folder = "../client/build"]
-struct Asset;
+// #[derive(RustEmbed)]
+// #[folder = "../client/build"]
+// struct Asset;
 
-fn handle_embedded_file(path: &str) -> HttpResponse {
-    match Asset::get(path) {
-        Some(content) => {
-            let body: Body = match content {
-                Cow::Borrowed(bytes) => bytes.into(),
-                Cow::Owned(bytes) => bytes.into(),
-            };
-            HttpResponse::Ok().content_type(mime_guess::from_path(path).first_or_octet_stream().as_ref()).body(body)
-        }
-        None => index(),
-    }
+// fn handle_embedded_file(path: &str) -> HttpResponse {
+//     match Asset::get(path) {
+//         Some(content) => {
+//             let body: Body = match content {
+//                 Cow::Borrowed(bytes) => bytes.into(),
+//                 Cow::Owned(bytes) => bytes.into(),
+//             };
+//             HttpResponse::Ok().content_type(mime_guess::from_path(path).first_or_octet_stream().as_ref()).body(body)
+//         }
+//         None => index(),
+//     }
+// }
+
+// pub fn index() -> HttpResponse {
+//     handle_embedded_file("index.html")
+// }
+//
+// pub fn dist(req: HttpRequest) -> HttpResponse {
+//     let path = &req.path()["/".len()..]; // trim the preceding `/` in path
+//     handle_embedded_file(path)
+// }
+
+
+// pub async fn graphql(
+//     schema: web::Data<Arc<graphql::Schema>>,
+//     pool: web::Data<DbPool>,
+//     request: web::Json<juniper::http::GraphQLRequest>,
+//     raw_request: HttpRequest, //needed to extract the http authorization bearer token
+// ) -> Result<HttpResponse, Error> {
+//     let token = match Authorization::<Bearer>::parse(&raw_request) {
+//         Ok(auth) => auth.into_scheme().token().to_string(),
+//         _ => "".to_string(), //no valid token
+//     };
+//     println!("user provided token: {}", token);
+//     let user = web::block(move || {
+//         let res = request.execute(&schema,
+//                                   &graphql::Context {
+//                                       db_conn: pool.get().expect("Couldn't get db connection from pool"),
+//                                       token,
+//                                   });
+//         Ok::<_, serde_json::error::Error>(serde_json::to_string(&res)?)
+//     })
+//         .await?;
+//     Ok(HttpResponse::Ok()
+//         .content_type("application/json")
+//         .body(user))
+// }
+
+pub(crate) async fn index(schema: web::Data<BooksSchema>, req: GQLRequest) -> GQLResponse {
+    req.into_inner().execute(&schema).await.into()
 }
 
-pub fn index() -> HttpResponse {
-    handle_embedded_file("index.html")
-}
-
-pub fn dist(req: HttpRequest) -> HttpResponse {
-    let path = &req.path()["/".len()..]; // trim the preceding `/` in path
-    handle_embedded_file(path)
-}
-
-pub fn graphiql() -> HttpResponse {
-    HttpResponse::Ok()
-        .content_type("text/html; charset=utf-8")
-        .body(juniper::http::playground::playground_source("/graphql"))
-}
-
-pub async fn graphql(
-    schema: web::Data<Arc<graphql::Schema>>,
-    pool: web::Data<DbPool>,
-    request: web::Json<juniper::http::GraphQLRequest>,
-    raw_request: HttpRequest, //needed to extract the http authorization bearer token
-) -> Result<HttpResponse, Error> {
-    let token = match Authorization::<Bearer>::parse(&raw_request) {
-        Ok(auth) => auth.into_scheme().token().to_string(),
-        _ => "".to_string(), //no valid token
-    };
-    println!("user provided token: {}", token);
-    let user = web::block(move || {
-        let res = request.execute(&schema,
-                                  &graphql::Context {
-                                      db_conn: pool.get().expect("Couldn't get db connection from pool"),
-                                      token,
-                                  });
-        Ok::<_, serde_json::error::Error>(serde_json::to_string(&res)?)
-    })
-        .await?;
+pub(crate) async fn index_playground() -> Result<HttpResponse> {
     Ok(HttpResponse::Ok()
-        .content_type("application/json")
-        .body(user))
+        .content_type("text/html; charset=utf-8")
+        .body(playground_source(
+            GraphQLPlaygroundConfig::new("/").subscription_endpoint("/"),
+        )))
 }
 
-#[derive(Debug, serde::Serialize, serde::Deserialize)]
-pub struct NewEvent {
-    pub property_id: String,
-    pub url: String,
-    pub user_agent: String,
-    pub fingerprint: String,
-    pub is_private: bool,
+pub(crate) async fn index_ws(
+    schema: web::Data<BooksSchema>,
+    req: HttpRequest,
+    payload: web::Payload,
+) -> Result<HttpResponse> {
+    ws::start_with_protocols(WSSubscription::new(&schema), &["graphql-ws"], &req, payload)
 }
-
-pub async fn event(pool: web::Data<DbPool>,
-                   request: web::Json<NewEvent>,
-                   raw_request: HttpRequest, //needed to extract the http authorization bearer token
-) -> HttpResponse {
-    let conn = pool.get().expect("Couldn't get db connection from pool");
-    // let token = match Authorization::<Bearer>::parse(&raw_request) {
-    //     Ok(auth) => auth.into_scheme().token().to_string(),
-    //     Err(_) => return HttpResponse::Unauthorized().finish()
-    // };
-    //
-    // let user_id = match crate::auth::get_user_id(&token) {
-    //     Ok(user_id) => user_id,
-    //     _ => return HttpResponse::Unauthorized().finish(),
-    // };
-    //
-    // if Property::is_property_id_belong_to_user_id(&conn, &request.property_id, user_id) != Ok(true) {
-    //     return HttpResponse::Unauthorized().finish();
-    // }
-    match Event::insert(&conn,
-                        &request.url,
-                        &raw_request.connection_info().host(),
-                        &request.user_agent,
-                        &request.fingerprint,
-                        request.is_private,
-                        &request.property_id) {
-        Ok(_) => HttpResponse::Ok().finish(),
-        Err(e) => {
-            eprintln!("Event insert error {}", e);
-            HttpResponse::Unauthorized().finish()
-        }
-    }
-}
-
-/*
-curl --header "Content-Type: application/json" \
-  --request POST \
-  --data '{"username":"xyz","password":"xyz"}' \
-  http://localhost:3000/api/login
- */
--- server/src/main.rs
+++ server/src/main.rs
@@ -10,38 +10,80 @@ mod db;
 mod graphql;
 mod handler;
 mod models;
+mod asyncgql;
 
-use actix_web::{web, App, HttpServer};
+use actix_web::{guard, web, App, HttpRequest, HttpResponse, HttpServer, Result};
+use actix_web_actors::ws;
+use async_graphql::http::{playground_source, GraphQLPlaygroundConfig};
+use async_graphql::Schema;
+use async_graphql_actix_web::{GQLRequest, GQLResponse, WSSubscription};
 use actix_cors::Cors;
+use asyncgql::{BooksSchema, MutationRoot, QueryRoot, Storage, SubscriptionRoot};
+use clap::{Arg, App as ClapApp, SubCommand};
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    dotenv::dotenv().ok();
-    let listener = std::env::var("IP_PORT").expect("IP_PORT must be set. Eg. 0.0.0.0:80");
+    let matches = ClapApp::new("yolotrader server")
+        .version("1.0")
+        .author("Eric Semeniuc <eric.semeniuc@gmail.com>")
+        .about("Backend server for yolotrader")
+        .arg(Arg::with_name("ip")
+            .long("ip")
+            .value_name("IP_ADDRESS")
+            .help("Listens on the provided interface")
+            .required(true))
+        .arg(Arg::with_name("port")
+            .short("p")
+            .long("port")
+            .value_name("PORT")
+            .help("Listens on the provided port")
+            .required(true))
+        .get_matches();
 
-    let schema = std::sync::Arc::new(graphql::create_schema());
+    // Gets a value for config if supplied by user, or defaults to "default.conf"
+    let config = matches.value_of("config").unwrap_or("default.conf");
+    println!("Value for config: {}", config);
+
+    let listener = std::env::var("IP_PORT").expect("IP_PORT must be set. Eg. 0.0.0.0:80");
     let pool = db::establish_connection();
     db::run_migrations(&pool.get().unwrap()).expect("Unable to run migrations");
+
+    let schema = Schema::build(QueryRoot, MutationRoot, SubscriptionRoot)
+        .data(Storage::default())
+        .finish();
+
+    println!("Playground: http://localhost:8000");
+
     HttpServer::new(move || {
-        let cors_rules = if cfg!(debug_assertions) {
-            Cors::default()
-        } else {
-            Cors::new()
-                .allowed_origin("https://decloak.io")
-                .allowed_methods(vec!["GET", "POST"])
-                .finish()
-        };
         App::new()
-            .wrap(cors_rules)
             .data(schema.clone())
-            .data(pool.clone())
-            .route("/event", web::post().to(handler::event))
-            .route("/graphql", web::post().to(handler::graphql))
-            .route("/graphiql", web::get().to(handler::graphiql))
-            .route("/", web::get().to(handler::index))
-            .route("/{_:.*}", web::get().to(handler::dist))
+            .service(web::resource("/").guard(guard::Post()).to(handler::index))
+            .service(
+                web::resource("/")
+                    .guard(guard::Get())
+                    .guard(guard::Header("upgrade", "websocket"))
+                    .to(handler::index_ws),
+            )
+            .service(web::resource("/").guard(guard::Get()).to(handler::index_playground))
     })
-        .bind(listener)?
+        .bind("127.0.0.1:8000")?
         .run()
         .await
+
+
+    // HttpServer::new(move || {
+    //
+    //     App::new()
+    //         .wrap(cors_rules)
+    //         .data(schema.clone())
+    //         .data(pool.clone())
+    //         .route("/event", web::post().to(handler::event))
+    //         .route("/graphql", web::post().to(handler::graphql))
+    //         .route("/graphiql", web::get().to(handler::graphiql))
+    //         .route("/", web::get().to(handler::index))
+    //         .route("/{_:.*}", web::get().to(handler::dist))
+    // })
+    //     .bind(listener)?
+    //     .run()
+    //     .await
 }
