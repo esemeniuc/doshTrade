122732cd9f261a6e3462e0c319313ca055f5ea3b
kai wen zhang <kaizhangwen@gmail.com>
2020-06-13 18:04:33 -0700

    add useSubscription logic

--- client/src/containers/StockListContainer.tsx
+++ client/src/containers/StockListContainer.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
+
 import Container from "@material-ui/core/Container";
 import {
     AppBar,
     Box,
     Toolbar, Typography
 } from "@material-ui/core";
+import {ApolloError, useSubscription} from '@apollo/client';
+import {loader} from 'graphql.macro';
+
+const STOCK_PRICES_SUBSCRIPTION = loader('../graphql/stockPrices.gql');
+
 
 function StockListContainer() {
+    const tickerSymbols = ["AAPL", "BANANA"]
+    const { data, loading, error } = useSubscription(
+        STOCK_PRICES_SUBSCRIPTION,
+        { variables: { tickerSymbols } }
+    );
+
+    if (loading) return <Typography variant="caption">
+        <Box textAlign="center">
+            loading ...
+        </Box>
+    </Typography>
+
+    if (error) return <Typography variant="caption">
+        <Box textAlign="center">
+            error!!
+        </Box>
+    </Typography>
 
     return (
         <Container component="main" maxWidth='sm'>
@@ -20,6 +43,11 @@ function StockListContainer() {
             <Typography variant="caption">
                 <Box textAlign="center">
                     Since close yesterday
+                </Box>
+            </Typography>
+            <Typography variant="caption">
+                <Box textAlign="center">
+                    {data}
                 </Box>
             </Typography>
         </Container>
