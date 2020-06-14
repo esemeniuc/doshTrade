63b5d074dddc5e4046c1fc1904cfa34986074706
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2020-06-14 15:36:36 -0700

    serde decapitalization

--- server/src/main.rs
+++ server/src/main.rs
@@ -21,21 +21,34 @@ use actix_cors::Cors;
 use asyncgql::{BooksSchema, MutationRoot, QueryRoot, Storage, SubscriptionRoot};
 use clap::{Arg, App as ClapApp, SubCommand};
 use actix_web::client::Client;
+use serde::{Serialize, Deserialize};
 
-async fn getter() {
-    let mut client = Client::default();
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Person {
+    latest_price: f64,
+    latest_volume: u64,
+    latest_update: u64,
+}
+
+async fn getter(tickers: &Vec<String>) -> Result<(), actix_web::Error> {
+    // std::env::set_var("RUST_LOG", "actix_http=trace");
+    let client = Client::default();
 
     // Create request builder and send request
-    let response = client.get("http://www.rust-lang.org")
-        .header("User-Agent", "Actix-web")
-        .send().await;                      // <- Send http request
-
-    println!("Response: {:?}", response);
+    let mut response = client
+        .get("https://sandbox.iexapis.com/stable/stock/twtr/quote?filter=latestPrice,latestVolume,latestUpdate&token=Tsk_2311e67e08f1404498c7a7fb91685839") // <--- notice the "s" in "https://..."
+        .send()
+        .await?; // <- Send http request
+    let body = response.body().await?;
+    let p: Person = serde_json::from_slice(body.as_ref())?;
+    println!("Downloaded: {:?} ", p);
+    Ok(())
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    getter().await;
+    let a = getter(&vec!["AAPL".to_string(), "GOOG".to_string()]).await;
 
 
     let matches = ClapApp::new("yolotrader server")
