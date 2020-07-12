66a37c79f53971c3fa97553ac4fb2d32a4b9acef
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2020-07-12 11:29:24 -0700

    make client table have non null fields, and uniqueness constraint

--- server/migrations/2020-07-12-161949_create_clients/up.sql
+++ server/migrations/2020-07-12-161949_create_clients/up.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS clients
 (
     id       INTEGER NOT NULL PRIMARY KEY,
     endpoint VARCHAR NOT NULL,
-    p256dh   VARCHAR,
-    auth     VARCHAR
+    p256dh   VARCHAR NOT NULL,
+    auth     VARCHAR NOT NULL,
+    UNIQUE (endpoint, p256dh, auth)
 );
