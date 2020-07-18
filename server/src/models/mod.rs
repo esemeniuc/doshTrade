c879074e1a25060e8358d4c721a9c8fa5a7380ac
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2020-07-18 08:16:36 -0700

    add missing import

--- server/src/models/mod.rs
+++ server/src/models/mod.rs
@@ -1,17 +1,18 @@
-pub mod stock;
-pub mod intraday_price;
 pub mod client;
 pub mod client_subscription;
 pub mod event;
+pub mod intraday_price;
 pub mod property;
+pub mod stock;
 pub mod user;
 
 //Models
 pub use client::Client;
 pub use client_subscription::ClientSubscription;
 pub use event::Event;
+pub use intraday_price::IntradayPrice;
 pub use property::Property;
-pub use user::User;
 pub use stock::Stock;
+pub use user::User;
 
 pub mod schema; //needed to add to this sub module otherwise no model can access schema
